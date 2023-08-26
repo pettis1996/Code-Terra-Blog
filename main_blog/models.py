6c8d813c632adb9aed29175e6c19fed70c88fb10
Paris Pettis <pettis_paris@hotmail.com>
2023-08-26 21:05:48 +0300

    update

--- main_blog/models.py
+++ main_blog/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
     
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    bio = RichTextField(max_length=1000, blank=True, null=True)
+    bio = models.TextField(max_length=1000)
     profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
     website_url = models.CharField(max_length=255, null=True, blank=True)
     github_url = models.CharField(max_length=255, null=True, blank=True)
@@ -34,7 +34,7 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE) ## if user gets deleted all user's posts get deleted.
     img_url = models.CharField(max_length=255, default="https://media.istockphoto.com/id/1047259374/photo/programming-source-code-abstract-background.jpg?b=1&s=612x612&w=0&k=20&c=ujRPoiaJlm5U3WDWcVVa1YVlFIt6Gcjr-RstzOEPbIU=")
-    body = RichTextField(blank=True, null=True)
+    body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='general')
     status = models.CharField(max_length=100, default='basic') # basic - featured - ad
--- main_blog/templatetags/blog_extras.py
+++ main_blog/templatetags/blog_extras.py
@@ -7,4 +7,4 @@ register = template.Library()
 
 @register.filter
 def convert_markdown(value):
-    return markdown.markdown(value, extensions=['markdown.extensions.fenced_code'])
+    return markdown.markdown(value)
